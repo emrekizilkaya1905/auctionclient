03930ae5c28e0c89b8279bdb4d262300c7ed7ddf
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2025-05-08 17:09:08 +0200

    Pagination is done

--- src/Pages/Vehicle/VehicleList.tsx
+++ src/Pages/Vehicle/VehicleList.tsx
@@ -5,22 +5,125 @@ import "./Styles/VehicleList.css";
 import Circle from "./Circle";
 import { Link } from "react-router-dom";
 import Banner from "./Banner";
+import { SD_FilterTypes } from "../../interfaces/enums/SD_FilterTypes";
 
 function VehicleList() {
   const { data, isLoading } = useGetVehiclesQuery(null);
-  const [vehicles, setVehiclesState] = useState<vehicleModel[]>([]);
+  const forPaginationArr: vehicleModel[] = [];
+  const [filterResponse, setFilterResponse] = useState<vehicleModel[]>([]);
+  const [result, setResultState] = useState<vehicleModel[]>([]);
+  const [vehicles, setVehicleState] = useState<vehicleModel[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>();
+  const defaultPaginationArr: number[] = [];
+  const filterOptions: Array<SD_FilterTypes> = [
+    SD_FilterTypes.NAME_A_Z,
+    SD_FilterTypes.NAME_Z_A,
+    SD_FilterTypes.PRICE_HIGH_LOW,
+    SD_FilterTypes.PRICE_LOW_HIGH,
+  ];
+  function handleFilterClick(sortTypes: any, index?: number) {
+    let forSortArray = [...result];
+    if (filterOptions[sortTypes] === SD_FilterTypes.PRICE_HIGH_LOW) {
+      forSortArray.sort((a, b) => {
+        return b.price - a.price;
+      });
+    }
+    if (filterOptions[sortTypes] === SD_FilterTypes.PRICE_LOW_HIGH) {
+      forSortArray.sort((a, b) => {
+        return a.price - b.price;
+      });
+    }
+    if (filterOptions[sortTypes] === SD_FilterTypes.NAME_A_Z) {
+      forSortArray.sort((a, b) =>
+        a.brandAndModel
+          .toLowerCase()
+          .localeCompare(b.brandAndModel.toLowerCase())
+      );
+    }
+    if (filterOptions[sortTypes] === SD_FilterTypes.NAME_Z_A) {
+      forSortArray.sort((a, b) =>
+        b.brandAndModel
+          .toLowerCase()
+          .localeCompare(a.brandAndModel.toLowerCase())
+      );
+    }
+    localStorage.setItem("myFilter", JSON.stringify(forSortArray));
+    if (filterOptions[sortTypes] === SD_FilterTypes.RemoveFilter) {
+      localStorage.removeItem("myFilter");
+    }
+    if (sortTypes === SD_FilterTypes.Pagination) {
+      setCurrentPage(index!);
+      const constantPage = 8;
+      forSortArray = [];
+      const reelResult = index! * constantPage;
+      for (
+        let index = reelResult;
+        index <= reelResult + constantPage - 1;
+        index++
+      ) {
+        if (data.result[index] !== undefined) {
+          forSortArray.push(data.result[index]);
+        }
+      }
+    }
+
+    setFilterResponse(forSortArray);
+  }
 
   useEffect(() => {
     if (data) {
-      setVehiclesState(data.result || []);
+      setVehicleState(data.result);
+      setResultState(data.result);
+      setFilterResponse(data.result);
     }
   }, [data]);
+  useEffect(() => {
+    const storedArray = JSON.parse(localStorage.getItem("myFilter")!);
+    if (data && storedArray === null) {
+      setFilterResponse(data.result);
+    }
+    if (storedArray !== null) {
+      setFilterResponse(storedArray);
+    }
+  }, [vehicles, data]);
+
+  const cellCalc = data && data.result ? Math.ceil(data.result.length / 8) : 0;
+  for (let index = 0; index < cellCalc; index++) {
+    defaultPaginationArr.push(index);
+  }
 
   return (
     <div className="container">
       <Banner></Banner>
       <div className="row">
-        {vehicles.map((vehicle: any, index: any) => {
+        <div className="dropdown mt-3">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Filter
+          </button>
+          <ul className="dropdown-menu">
+            {filterOptions.map((filterTypes, index) => {
+              return (
+                <>
+                  <li>
+                    <a
+                      className="dropdown-item"
+                      onClick={() => handleFilterClick(index)}
+                    >
+                      {" "}
+                      {filterTypes}{" "}
+                    </a>
+                  </li>
+                </>
+              );
+            })}
+          </ul>
+        </div>
+        {filterResponse.map((vehicle: any, index: any) => {
           return (
             <div className="col" key={index}>
               <div className="auction-card text-center">
@@ -59,6 +162,54 @@ function VehicleList() {
           );
         })}
       </div>
+      <ul className="pagination">
+        <li className={`page-item ${currentPage === 0 ? "disabled" : ""}`}>
+          <a
+            className="page-link"
+            href="#"
+            onClick={() => {
+              if (currentPage! > 0) {
+                handleFilterClick("Pagination", currentPage! - 1);
+              }
+            }}
+          >
+            Previous
+          </a>
+        </li>
+
+        {defaultPaginationArr.map((key, index) => (
+          <li
+            className={`page-item ${currentPage === index ? "active" : ""}`}
+            key={index}
+          >
+            <a
+              className="page-link"
+              href="#"
+              onClick={() => handleFilterClick("Pagination", index)}
+            >
+              {key + 1}
+            </a>
+          </li>
+        ))}
+
+        <li
+          className={`page-item ${
+            currentPage === defaultPaginationArr.length - 1 ? "disabled" : ""
+          }`}
+        >
+          <a
+            className="page-link"
+            href="#"
+            onClick={() => {
+              if (currentPage! < defaultPaginationArr.length - 1) {
+                handleFilterClick("Pagination", currentPage! + 1);
+              }
+            }}
+          >
+            Next
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
